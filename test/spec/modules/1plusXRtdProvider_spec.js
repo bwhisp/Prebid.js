b602fbe70885b383a5f105cd1b3cc278121ba812
Anass Seddiki <anass.seddiki@1plusx.com>
2022-06-07 15:12:57 +0000

    Unit tests empty shell

---
+++ test/spec/modules/1plusXRtdProvider_spec.js
@@ -0,0 +1,12 @@
+import { config } from 'src/config';
+import { onePlusXSubmodule } from 'modules/1plusXRtdProvider';
+
+describe('1plusXRtdProvider', () => {
+  before(() => { })
+
+  after(() => { })
+
+  describe('onePlusXSubmodule', () => { })
+
+
+})
