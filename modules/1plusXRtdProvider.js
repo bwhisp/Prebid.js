d6f15adcdf001eb21b0e3b4a5fdc3ed95b5be071
Anass Seddiki <anass.seddiki@1plusx.com>
2022-06-08 12:43:51 +0000

    Add params to functions of the module

--- modules/1plusXRtdProvider.js
+++ modules/1plusXRtdProvider.js
@@ -4,8 +4,10 @@ const MODULE_NAME = '1plusX'
 // Functions
 
 // Functions exported in submodule object
-const init = () => { }
-const getBidRequestData = () => { }
+const init = (config, userConsent) => { }
+
+const getBidRequestData = (reqBidsConfigObj, callback, config, userConsent) => { }
+
 // The RTD submodule object to be exported
 export const onePlusXSubmodule = {
   name: MODULE_NAME,
