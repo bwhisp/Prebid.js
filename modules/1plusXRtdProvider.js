2592a0d6473b00d40a8db2350538f2bd351933b0
Anass Seddiki <anass.seddiki@1plusx.com>
2022-06-10 13:03:46 +0000

    Fix tests...

--- modules/1plusXRtdProvider.js
+++ modules/1plusXRtdProvider.js
@@ -1,8 +1,6 @@
 import { submodule } from '../src/hook.js'
 import { ajax } from '../src/ajax.js'
 import { logMessage, logError, deepAccess, isNumber } from '../src/utils.js';
-import { resolve } from 'core-js/fn/promise';
-import { reject } from 'lodash';
 
 // Constants
 const REAL_TIME_MODULE = 'realTimeData';
@@ -11,36 +9,40 @@ const PAPI_VERSION = 'v1.0';
 
 // Functions
 const extractConfig = (config) => {
-  // CustomerId 
-  const customerId = config;
+  // CustomerId
+  const customerId = deepAccess(config, 'params.customerId');
   if (!customerId) {
     throw new Error('REQUIRED CUSTOMER ID');
   }
   // Timeout
   const tempTimeout = deepAccess(config, 'params.timeout');
-  const timeout = isNumber(tempTimeout) && tempTimeout > 300 ? timeout : 1000;
+  const timeout = isNumber(tempTimeout) && tempTimeout > 300 ? tempTimeout : 1000;
 
   return { customerId, timeout };
 }
 
 const getPapiUrl = ({ customerId }) => {
+  logMessage('GET PAPI URL');
   // https://[yourClientId].profiles.tagger.opecloud.com/[VERSION]/targeting?url=
   const currentUrl = encodeURIComponent(window.location.href);
   const papiUrl = `https://${customerId}.profiles.tagger.opecloud.com/${PAPI_VERSION}/targeting?url=${currentUrl}`;
   return papiUrl;
 }
 
-getTargetingDataFromPapi = (papiUrl) => {
-  return new Promise(
+const getTargetingDataFromPapi = (papiUrl) => {
+  return new Promise((resolve, reject) => {
     ajax(papiUrl, {
       success(responseText, response) {
-        resolve(response);
+        logMessage(responseText);
+        resolve(response.response);
       },
       error(errorText, error) {
+        console.log(errorText)
+        console.log(JSON.stringify(error, null, 2))
         reject(error);
       }
     })
-  )
+  })
 }
 
 // Functions exported in submodule object
@@ -50,23 +52,30 @@ const init = (config, userConsent) => {
 }
 
 const getBidRequestData = (reqBidsConfigObj, callback, config, userConsent) => {
-  // Get the required config
-  const { customerId, timeout } = extractConfig(config);
-  // Get PAPI URL
-  const papiUrl = getPapiUrl({ customerId })
-  // Call PAPI
-  getTargetingDataFromPapi(papiUrl)
-    .then((response) => {
-      // -- Then :
-      // ---- extract relevant data
-      // ---- set the data to the bid
-      callback();
-    })
-    .catch((error) => {
-      // -- Catch : print err & do nothing
-      logError(error);
-      callback();
-    })
+  try {
+    // Get the required config
+    const { customerId } = extractConfig(config);
+    // Get PAPI URL
+    const papiUrl = getPapiUrl({ customerId })
+    // Call PAPI
+    getTargetingDataFromPapi(papiUrl)
+      .then((response) => {
+        // -- Then :
+        // ---- extract relevant data
+        // ---- set the data to the bid
+        console.log('REQUEST TO PAPI SUCCESS');
+        callback();
+      })
+      .catch((error) => {
+        // -- Catch : print err & do nothing
+        console.log('REQUEST TO PAPI ERROR');
+        // logError(error);
+        callback();
+      })
+  } catch (error) {
+    logError(error);
+    callback();
+  }
 }
 
 // The RTD submodule object to be exported
